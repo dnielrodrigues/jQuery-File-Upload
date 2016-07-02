4b7799ff6b29681900698b41840c240921a6f044
Daniel Rodrigues de Sousa <danielrodrigues.a113@gmail.com>
2016-07-02 07:44:02 -0300

    fix bug

--- server/php/index.php
+++ server/php/index.php
@@ -10,6 +10,8 @@
  * http://www.opensource.org/licenses/MIT
  */
 
-error_reporting(E_ALL | E_STRICT);
+// error_reporting(E_ALL | E_STRICT); // ???
 require('UploadHandler.php');
 $upload_handler = new UploadHandler();
+
+
